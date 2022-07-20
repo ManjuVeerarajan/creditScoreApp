959e6dbcab5bc40e972f06a2524a40231f0b9485
Manju Veerarajan <manju@mobypay.my>
2022-07-20 22:14:24 +0530

    api key fix

--- src/main/java/my/mobypay/creditScore/APISecurityConfig.java
+++ src/main/java/my/mobypay/creditScore/APISecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,7 +25,8 @@ import my.mobypay.creditScore.dao.Creditcheckersysconfig;
 import my.mobypay.creditScore.repository.CreditCheckerAuthRepository;
 
 @Configuration
-//@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 //@Order(1)
 public class APISecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -36,76 +38,10 @@ public class APISecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity httpSecurity) throws Exception {
-		// APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
-		APIKeyAuthFilter filter = new APIKeyAuthFilter(getKeyValueFromDBString(), getKeySecretValueFromDB());
-		filter.setAuthenticationManager(new AuthenticationManager() {
-//        	getAuthEnableDetailsFromDB();
 
-			@Override
-			public Authentication authenticate(Authentication authentication) throws BadCredentialsException {
-				String authEnableDetailsFromDB = getAuthEnableDetailsFromDB();
-				if (StringUtils.equalsIgnoreCase(getAuthEnableDetailsFromDB(), "1")) {
-					String principal = (String) authentication.getPrincipal();
-
-					String[] secretfromDb = getSecretValueFromDB();
-					List<String> secretList = Arrays.asList(secretfromDb);
-
-					if (!secretList.contains(principal)) {
-						throw new BadCredentialsException("401");
-					}
-					/*
-					 * if (!principalRequestValue.equals(principal)) { throw new
-					 * BadCredentialsException("401"); }
-					 */
-					authentication.setAuthenticated(true);
-					return authentication;
-				} else {
-					authentication.setAuthenticated(true);
-					return authentication;
-				}
-			}
-		});
 		httpSecurity.antMatcher("/**").csrf().disable().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().addFilter(filter).authorizeRequests()
-				.anyRequest().authenticated();
-	}
-
-	@Bean
-	public HashMap<String, String> getKeySecretValueFromDB() {
-		HashMap<String, String> keyMap = new HashMap<String, String>();
-		List<CreditCheckerAuthDao> configValues = creditCheckerAuthRepository.findAll();
-		String[] keyfromDb = new String[configValues.size() + 1];
-		System.out.println("Size " + configValues.size());
-		for (int i = 0; i < configValues.size(); i++) {
-			keyMap.put(configValues.get(i).getApi_key().toLowerCase(), configValues.get(i).getApi_secret());
-		}
-		return keyMap;
-	}
-
-	/*
-	 * @Bean public HashMap<String,String> getSecretValueFromDB() {
-	 * HashMap<String,String> secretMap = new HashMap<String,String>();
-	 * List<PaymentProcessorAuthDao> configValues =
-	 * paymentProcessorAuthRepository.findAll(); System.out.println("configValues 1"
-	 * +configValues.get(0).getApi_key()); System.out.println("Size "
-	 * +configValues.size()); for (int i=0 ;i<configValues.size(); i++) {
-	 * secretMap.put(configValues.get(i).getId(),configValues.get(i).getApi_secret()
-	 * ); } System.out.println("secretMap " +secretMap);
-	 * 
-	 * return secretMap; }
-	 */
-	public String[] getKeyValueFromDBString() {
-		HashMap<String, String> keyMap = new HashMap<String, String>();
-
-		List<CreditCheckerAuthDao> configValues = creditCheckerAuthRepository.findAll();
-		String[] keyfromDb = new String[configValues.size() + 1];
-		System.out.println("Size " + configValues.size());
-		for (int i = 0; i < configValues.size(); i++) {
-			keyfromDb[i] = configValues.get(i).getApi_key().toLowerCase();
-		}
-		System.out.println("keyfromDb " + keyfromDb);
-
-		return keyfromDb;
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().addFilter(tokenProcessingFilter())
+				.authorizeRequests().anyRequest().authenticated();
 	}
 
 	@Bean
@@ -128,6 +64,33 @@ public class APISecurityConfig extends WebSecurityConfigurerAdapter {
 		String configValues = creditScoreConfigRepository.findValueFromName(GlobalConstants.PLATFORM_AUTH);
 		return configValues;
 
+	}
+
+	@Bean
+	APIKeyAuthFilter tokenProcessingFilter() throws Exception {
+		APIKeyAuthFilter tokenProcessingFilter = new APIKeyAuthFilter();
+		tokenProcessingFilter.setAuthenticationManager(new AuthenticationManager() {
+			@Override
+			public Authentication authenticate(Authentication authentication) throws BadCredentialsException {
+				String authEnableDetailsFromDB = getAuthEnableDetailsFromDB();
+				if (StringUtils.equalsIgnoreCase(authEnableDetailsFromDB, "1")) {
+					String principal = (String) authentication.getPrincipal();
+
+					String[] secretfromDb = getSecretValueFromDB();
+					List<String> secretList = Arrays.asList(secretfromDb);
+
+					if (!secretList.contains(principal)) {
+						throw new BadCredentialsException("401");
+					}
+					authentication.setAuthenticated(true);
+					return authentication;
+				} else {
+					authentication.setAuthenticated(true);
+					return authentication;
+				}
+			}
+		});
+		return tokenProcessingFilter;
 	}
 
 }
